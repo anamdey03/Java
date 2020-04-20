2fc655f294a36f3ef1326b229687b2753d26fae5
Anamitra Dey <anamitradey.ece@gmail.com>
2020-04-19 23:33:05 -0400

    Added Bodmas file

---
+++ src/com/practice/maths/Bodmas.java
@@ -0,0 +1,15 @@
+package com.practice.maths;
+
+public class Bodmas {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int c = 3;
+        int d = 2;
+
+        int bodmas = ((a+b)*c)/d;
+        System.out.println("Output of the expression is "+bodmas);
+    }
+}
