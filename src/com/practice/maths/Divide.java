b4840c6d7fa33aae3fad2e0406cc35146c9e7876
Anamitra Dey <anamitradey.ece@gmail.com>
2020-04-19 20:31:04 -0400

    first commit

---
+++ src/com/practice/maths/Divide.java
@@ -0,0 +1,13 @@
+package com.practice.maths;
+
+public class Divide {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int divide = a / b;
+        System.out.println("Divide of a and b is "+ divide);
+    }
+}
---
+++ src/com/practice/maths/Multiply.java
@@ -0,0 +1,13 @@
+package com.practice.maths;
+
+public class Multiply {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        int multiply = a * b;
+        System.out.println("Multiply of a and b is " + multiply);
+    }
+}
